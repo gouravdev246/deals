9f776ed63bf9efe389f27b1577d421984133709e
Gourav <132838923+gouravdev246@users.noreply.github.com>
2026-03-10 13:50:35 +0530

    DB Connection Done

--- deals-backend/server.js
+++ deals-backend/server.js
@@ -1,7 +1,10 @@
 const app = require("./src/app");
 const dotenv = require("dotenv");
+const connectDB = require("./src/db/connectDB");
 
 dotenv.config();
+
+connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
