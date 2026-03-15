16fc98e7b4f73ea3b8575c1ff2f550c1ae0b9de6
Gourav <132838923+gouravdev246@users.noreply.github.com>
2026-03-15 13:29:19 +0530

    Fix Term & Condition

--- lpudeals/components/RegisterPage.jsx
+++ lpudeals/components/RegisterPage.jsx
@@ -133,7 +133,7 @@ function Registration({ ...props }) {
                                 <input type="password" name="confirmPassword" id="confirmPassword" value={formData.confirmPassword} onChange={handleChange} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
                             <div>
-                                <input type="checkbox" /> <Link href="/terms" className="hover:text-orange-600 transition-colors">Terms & Conditions</Link>
+                                <input type="checkbox" /> Terms & Condition
                                 
                             </div>
                             
